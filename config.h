36c67be600b41c860a8361e056386950ff37cdc0
yarob-0 <yarob.021@gmail.com>
2022-04-18 13:52:01 +0200

    the amazing "keyboard select mode" patch done

--- config.h
+++ config.h
@@ -240,10 +240,11 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ ControlMask,          XK_k,           kscrollup,      {.i = 2} },
-	{ ControlMask,          XK_j,           kscrolldown,    {.i = 2} },
+	{ ControlMask,          XK_k,           kscrollup,      {.i =  2} },
+	{ ControlMask,          XK_j,           kscrolldown,    {.i =  2} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_Escape,      keyboard_select,{.i =  0} },
 };
 
 /*
